13760524a800bf1e5bb064f9e4be19243740ec5a
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-09-13 14:31:46 +0530

    Update __main__.py

--- dreamkiller/__main__.py
+++ dreamkiller/__main__.py
@@ -876,7 +876,7 @@ def main():
 
     donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
-    dispatcher.add_handler(Music_handler)
+    dispatcher.add_handler(Music_callback_handler)
     dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
